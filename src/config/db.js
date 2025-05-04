cf86c0ec10c134ac3065f208a44c963114cd5d0f
LAPTOP-SGVCVDCM\Hiranya <iduwara123lk@gmail.com>
2025-05-04 02:21:37 +0100

    db config chnages

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 
 const ConnectToMongo = async () => {
   try {
-    await mongoose.connect(process.env.CONNECTIONURL);
-    console.log('Connected to MongoDB');
+    await mongoose.connect(process.env.CONNECTIONURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
 };
+
 
 module.exports = { ConnectToMongo, mongoose };
